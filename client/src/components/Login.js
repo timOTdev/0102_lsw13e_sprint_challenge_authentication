22338b84ca54b4d0365197099e020713d2928945
Timothy Hoang <timh1203@gmail.com>
2018-10-19 13:35:43 -0400

    Add login feature to Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
 import React, { Component } from "react";
+import axios from 'axios'
 
 class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const endpoint = 'http://localhost:9000/api/login'
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem('jwt', res.data.token)
+        this.props.history.push('/api/jokes')
+      })
+      .catch(err => console.log(err))
+  } 
+
   render() {
     return (
-      <div>
-        <h1>Login CC</h1>
-      </div>
-    );
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username </label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password </label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    )
   }
 }
 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -39,7 +39,7 @@ class Register extends Component {
         <div>
           <label htmlFor="password">Password </label>
           <input
-            type="text"
+            type="password"
             name="password"
             value={this.state.password}
             onChange={this.handleChange}
