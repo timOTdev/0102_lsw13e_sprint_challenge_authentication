edfea229226c20e13905c77e84c088c7d7e2d8d2
Timothy Hoang <timh1203@gmail.com>
2018-10-19 13:16:39 -0400

    Add text to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home FC</h1>
+      <h1>The only goal is to make eyes roll</h1>
+      <p>What we have here is a wise-guy application. Dad jokes are all the rage these days.</p>
+      <p>Trust me, we all need these dad jokes in our lives.</p>
     </div>
   );
 }
