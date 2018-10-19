79a3df2487acdc926f8a35f60a62077158000d38
Timothy Hoang <timh1203@gmail.com>
2018-10-19 13:45:11 -0400

    Add jokes render to Jokes

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,13 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import styled from 'styled-components'
+import axios from 'axios'
 
 class Jokes extends Component {
+  state = {
+    jokes: [],
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt')
+    const endpoint = 'http://localhost:9000/api/jokes'
+    const options = {
+      headers: {
+        Authorization: token,
+      }
+    }
+    axios
+      .get(endpoint, options)
+      .then(res => {
+        this.setState({ jokes: res.data })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div>
-        <h1>Jokes FC</h1>
+        <H1a>Jokes List</H1a>
+        <Ul1>
+          {
+            this.state.jokes.map(jokes => (
+            <Fragment key={jokes.id}>
+              <Li1>{jokes.setup} <Span1>{jokes.punchline}</Span1></Li1>
+            </Fragment>
+            ))
+          }
+        </Ul1>
       </div>
-    );
+    )
   }
 }
+
+const H1a = styled.h1``
+const Ul1 = styled.ul`
+  list-style-type: none;
+`
+const Li1 = styled.li`
+  font-size: 2.4rem;
+`
+const Span1 = styled.span`
+  font-size: 1.6rem;
+`
 
 export default Jokes;
